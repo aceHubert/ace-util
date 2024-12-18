181a46459a0c125393ada6afb9731cbb00408b52
hubert <hubert@lejian.com>
2024-12-18 14:58:54 +0800

    fix: type rix

--- packages/injects/src/types.ts
+++ packages/injects/src/types.ts
@@ -25,7 +25,7 @@ export type InjectResult<F extends (...args: any) => any> = {
    * @param value Value to filter
    * @param args Arguments
    */
-  filter(value: Parameters<F>, ...args: GetRestArgs<Parameters<F>>): Promise<Parameters<F>>;
+  filter(value: Parameters<F>[0], ...args: GetRestArgs<Parameters<F>>): Promise<Parameters<F>>;
 
   /**
    * Execute from injection tag, won't have a return value
